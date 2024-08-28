a8047943083774c2eb45c599e6053bd659a4768c
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-28 10:20:04 -0300

    conexão: process.env

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,7 +1,10 @@
 const knex = require('knex')({
   client: "pg",
-  connection:{
-    connectionString: process.env.POSTGRES_URL
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   }
 })
 module.exports = knex
