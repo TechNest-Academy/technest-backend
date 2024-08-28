93dc2c682122f74cfdec05976d8f1d16de7f5ef8
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-28 06:49:51 -0300

    adicionando swagger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 const rotas = require('./routers/rotas');
 const app = express();
 const port = process.env.PORT;
-app.use(express.json());
 
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(rotas);
 
 app.listen(port,() => {
