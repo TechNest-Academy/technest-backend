ee2eb7ebca3fbacd24b836bf5f326c037faf22c8
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-28 02:28:51 -0300

    adicionando o controller alunos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const rotas = require('./routers/rotas');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use(express.json());
 
+app.use(rotas);
 
 app.listen(port,() => {
   console.log(`Rodando na url: http://localhost:${port}`);
