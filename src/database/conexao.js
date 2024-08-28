ec64da4b8467beaa1d7f48d5ca43c59dab04b2e3
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-28 11:13:50 -0300

    adicionando ssl e refatorando os nomes da conexão

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -13,18 +13,3 @@ const knex = require('knex')({
   }
 })
 module.exports = knex
-require('dotenv').config();
-const knex = require('knex')({
-  client: "pg",
-  connection: {
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-    ssl:{
-      rejectUnauthorized: false,
-    }
-  }
-})
-module.exports = knex
