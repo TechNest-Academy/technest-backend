cfe8147118b0ff47b4669f503e3f0b13570fdc44
Ewerton R F Bertoldo <97854973+EwertonRafael@users.noreply.github.com>
2024-10-21 17:02:29 -0300

    projeto Generation

---
+++ src/database/conexao.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const knex = require('knex')({
+  client: "pg",
+  connection: {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+    ssl:{
+      rejectUnauthorized: false,
+    }
+  }
+})
+module.exports = knex
