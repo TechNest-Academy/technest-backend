abd680fdb3445e86ab8c6e988c50da7b00b614f3
Ewerton Rafael <ton_rafael@hotmail.com>
2024-10-22 18:14:59 -0300

    criando migration com prismaORM

--- src/midware/intermediario.js
+++ src/midware/intermediario.js
@@ -1,33 +1,22 @@
-const knex = require('../database/conexao');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const validarCampos = async (req, res, next) => {
   try {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const { nome, idade, email, notaprimeiromodulo, notasegundomodulo } = req.body;
-    
-    if (!nome || !idade || !email || notaprimeiromodulo === undefined || notasegundomodulo === undefined) {
+    if (!nome || !idade || !email || !notaprimeiromodulo || !notasegundomodulo) {
       return res.status(400).json({ mensagem: "Todos os campos devem ser preenchidos." });
-    }
-
-    if (notaprimeiromodulo < 0 || notaprimeiromodulo > 10 || notasegundomodulo < 0 || notasegundomodulo > 10) {
+    }if (notaprimeiromodulo < 0 || notaprimeiromodulo > 10 || notasegundomodulo < 0 || notasegundomodulo > 10) {
       return res.status(400).json({ mensagem: 'A nota deve estar entre 0 e 10.' });
-    }
-
-    if (idade <= 0) {
+    }if (!idade || idade <= 0) {
       return res.status(400).json({ mensagem: "Idade deve ser um valor positivo." });
-    }
-
-    if (nome.length < 3) {
+    }if (nome.length < 3) {
       return res.status(400).json({ mensagem: "Nome deve ter pelo menos 3 letras." });
-    }
-
-    if (!emailRegex.test(email)) {
+    }if (!emailRegex.test(email)) {
       return res.status(400).json({ mensagem: "Email inválido." });
-    }
-
-    next();
+    }next();
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ mensagem: "Erro interno de servidor." });
   }
 };
@@ -40,44 +29,49 @@ const idValido = async (req, res, next) => {
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno." });
   }
-}
+};
 
 const listaVazia = async (req, res, next) => {
   try {
-    const todosAlunos = await knex("alunos").select("*");
+    const todosAlunos = await prisma.aluno.findMany();
     if (todosAlunos.length === 0) return res.status(200).json("Lista vazia");
     next();
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ mensagem: "erro interno." });
+    return res.status(500).json({ mensagem: "Erro interno." });
   }
-}
+};
 
 const emailValidoParaCadastro = async (req, res, next) => {
   try {
     const { email } = req.body;
-    const emailExistente = await knex("alunos").where( "email", email).first();
-    if (emailExistente) return res.status(400).json({ mensagem: 'Email ja existente.' });
+    const emailExistente = await prisma.aluno.findUnique({
+      where: { email },
+    });
+    if (emailExistente) return res.status(400).json({ mensagem: 'Email já existente.' });
     next();
   } catch (error) {
     console.log(error);
-    
-    return res.status(500).json({ mensagem: "erro interno." });
+    return res.status(500).json({ mensagem: "Erro interno." });
   }
-}
+};
+
 const emailValidoParaAtualizacao = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { email } = req.body;
-    const emailExistente = await knex("alunos").where( "email", email).andWhere("id", "!=", id).first();
-    if (emailExistente) return res.status(400).json({ mensagem: 'Email ja existente.' });
+    const emailExistente = await prisma.aluno.findFirst({
+      where: {
+        email,
+        id: { not: Number(id) },
+      },
+    });
+    if (emailExistente) return res.status(400).json({ mensagem: 'Email já existente.' });
     next();
   } catch (error) {
     console.log(error);
-    
-    return res.status(500).json({ mensagem: "erro interno." });
+    return res.status(500).json({ mensagem: "Erro interno." });
   }
-}
+};
 
 module.exports = {
   validarCampos,
