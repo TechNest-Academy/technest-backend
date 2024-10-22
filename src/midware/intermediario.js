3f6a1567be23220d3cc4d93f67fd5ddb7b281d95
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-22 14:23:23 -0300

    hotfix: fixed database

--- src/midware/intermediario.js
+++ src/midware/intermediario.js
@@ -5,18 +5,26 @@ const validarCampos = async (req, res, next) => {
   try {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const { nome, idade, email, notaprimeiromodulo, notasegundomodulo } = req.body;
-    if (!nome || !idade || !email || !notaprimeiromodulo || !notasegundomodulo) {
+    
+    if (!nome || !idade || !email || notaprimeiromodulo === undefined || notasegundomodulo === undefined) {
       return res.status(400).json({ mensagem: "Todos os campos devem ser preenchidos." });
-    }if (notaprimeiromodulo < 0 || notaprimeiromodulo > 10 || notasegundomodulo < 0 || notasegundomodulo > 10) {
+    }
+
+    if (notaprimeiromodulo < 0 || notaprimeiromodulo > 10 || notasegundomodulo < 0 || notasegundomodulo > 10) {
       return res.status(400).json({ mensagem: 'A nota deve estar entre 0 e 10.' });
-    }if (!idade || idade <= 0) {
+    }
+
+    if (idade <= 0) {
       return res.status(400).json({ mensagem: "Idade deve ser um valor positivo." });
     }if (nome.length < 3) {
       return res.status(400).json({ mensagem: "Nome deve ter pelo menos 3 letras." });
-    }if (!emailRegex.test(email)) {
+    }
+
+    if (!emailRegex.test(email)) {
       return res.status(400).json({ mensagem: "Email inválido." });
     }next();
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ mensagem: "Erro interno de servidor." });
   }
 };
