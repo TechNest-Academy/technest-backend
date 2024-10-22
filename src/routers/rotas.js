3e3019ef6a7eec7537f2d04ad378d636ec1ce714
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-22 19:26:16 -0300

    feat: routes to turma entity

--- src/routers/rotas.js
+++ src/routers/rotas.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const rotas = express();
 
-const { cadastrarAluno, listarAlunos, detalharAluno, atualizarAluno, deletarAluno } = require("../controllers/alunos");
 const { validarCampos, idValido, listaVazia, emailValidoParaCadastro, emailValidoParaAtualizacao } = require('../midware/intermediario');
+const { cadastrarAluno, listarAlunos, detalharAluno, atualizarAluno, deletarAluno } = require("../controllers/alunos");
 
 rotas.post("/cadastrar", validarCampos, emailValidoParaCadastro, cadastrarAluno);
 rotas.get("/listar", listaVazia, listarAlunos);
 rotas.get("/detalhar/:id", idValido, detalharAluno);
 rotas.put("/atualizar/:id", validarCampos, idValido, emailValidoParaAtualizacao, atualizarAluno);
 rotas.delete("/excluir/:id", idValido, deletarAluno);
+
+const { listarTurmas, detalharTurma, cadastrarTurma, atualizarTurma, deletarTurma } = require("../controllers/turma");
+
+rotas.get("/turmas", listarTurmas);
+rotas.get("/turma/:id", detalharTurma);
+rotas.post("/turma", cadastrarTurma);
+rotas.put("/turma/:id", atualizarTurma);
+rotas.delete("/turma/:id", deletarTurma);
 
 module.exports = rotas;
