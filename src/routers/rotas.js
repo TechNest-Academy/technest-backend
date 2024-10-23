b2b25553bb1349d19ae66396667e940efd080c4f
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-23 08:58:02 -0300

    fix:routes

--- src/routers/rotas.js
+++ src/routers/rotas.js
@@ -4,15 +4,15 @@ const rotas = express();
 const { validarCampos, idValido, listaVazia, emailValidoParaCadastro, emailValidoParaAtualizacao } = require('../midware/intermediario');
 const { cadastrarAluno, listarAlunos, detalharAluno, atualizarAluno, deletarAluno } = require("../controllers/alunos");
 
-rotas.post("/cadastrar", validarCampos, emailValidoParaCadastro, cadastrarAluno);
-rotas.get("/listar", listaVazia, listarAlunos);
-rotas.get("/detalhar/:id", idValido, detalharAluno);
-rotas.put("/atualizar/:id", validarCampos, idValido, emailValidoParaAtualizacao, atualizarAluno);
-rotas.delete("/excluir/:id", idValido, deletarAluno);
+rotas.post("aluno/cadastrar", validarCampos, emailValidoParaCadastro, cadastrarAluno);
+rotas.get("aluno/listar", listaVazia, listarAlunos);
+rotas.get("aluno/detalhar/:id", idValido, detalharAluno);
+rotas.put("aluno/atualizar/:id", validarCampos, idValido, emailValidoParaAtualizacao, atualizarAluno);
+rotas.delete("aluno/excluir/:id", idValido, deletarAluno);
 
 const { listarTurmas, detalharTurma, cadastrarTurma, atualizarTurma, deletarTurma } = require("../controllers/turma");
 
-rotas.get("/turmas", listarTurmas);
+rotas.get("/turma", listarTurmas);
 rotas.get("/turma/:id", detalharTurma);
 rotas.post("/turma", cadastrarTurma);
 rotas.put("/turma/:id", atualizarTurma);
